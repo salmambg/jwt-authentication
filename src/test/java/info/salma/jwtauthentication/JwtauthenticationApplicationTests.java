b4c7c2aaaf19c3efba89b12872ea9f4e8b686cb5
Salma Akter <gsalmahossain@gmail.com>
2024-08-18 09:21:33 +0200

    initiation of jwt authentication

---
+++ src/test/java/info/salma/jwtauthentication/JwtauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package info.salma.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
